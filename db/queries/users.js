12263df3fc95a501f7431eb8b8d4fd0c85588711
Kate Galich <kate.galich@outlook.com>
2022-10-26 22:31:10 -0400

    implementing login, logout

--- db/queries/users.js
+++ db/queries/users.js
@@ -7,4 +7,27 @@ const getUsers = () => {
     });
 };
 
-module.exports = { getUsers };
+const getUser = (userId) => {
+  let sql = 'SELECT * FROM users WHERE id=$1;';
+  let values = [userId];
+
+  return db.query(sql, values)
+    .then((result) => {
+      return result.rows[0];
+    });
+};
+
+const getCurrentUser = function(req) {
+  return getUser(req.session.user_id);
+};
+
+const getUserByEmail = function(email) {
+  let sql = 'SELECT * FROM users WHERE email=$1;';
+  let values = [email];
+
+  return db.query(sql, values)
+    .then((result) => {
+      return result.rows[0];
+    });
+};
+module.exports = { getUsers, getUser, getCurrentUser, getUserByEmail };
