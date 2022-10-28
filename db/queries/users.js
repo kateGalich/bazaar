7db613a44d73c75d44b3ec2ba6b435fe15fcdf74
Andrei Chirita <adchir95@gmail.com>
2022-10-28 15:34:09 +0000

    small css changes to login page

--- db/queries/users.js
+++ db/queries/users.js
@@ -34,7 +34,7 @@ const getUserByEmail = function(email) {
 const registerUser = function(user) {
   const userInfo = [`${user.name}`, `${user.password}`, `${user.email}`, `${user.phone}`, `${user.address}`];
 
-  return pool
+  return db
     .query(`
     INSERT INTO users (name, password, email, phone, address)
     VALUES ($1, $2, $3, $4, $5)
